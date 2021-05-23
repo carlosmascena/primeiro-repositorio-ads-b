215a68852533097efacc1869222afb18323f6301
carlosmascena <carlos.jsantos@bandtec.com.br>
2021-05-23 14:13:39 -0300

    Projeto ac3

---
+++ sysight/src/main/java/br/com/sysight/App.java
@@ -0,0 +1,52 @@
+package br.com.sysight;
+
+import java.io.IOException;
+import java.net.UnknownHostException;
+import org.json.JSONObject;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class App {
+     public static void main(String args[]) throws UnknownHostException, IOException, InterruptedException {
+       JSONObject json = new JSONObject();
+       
+       //json.put("text", "Olá! Eu sou a Sys! A bot da Sysight!");
+       //Slack.enviarMensagem(json);
+        
+        Conexao con = new Conexao();
+        JdbcTemplate config = new JdbcTemplate(con.getDatasource());
+
+        Recurso recurso = new Recurso();
+
+        // Parametros para o primeiro insert
+   
+        String momento =  recurso.getMomento();
+        String consumoRam = String.format("%.2f", recurso.getConsumoRam());
+        String consumoCpu = String.format("%.2f", recurso.getConsumoCPU());
+        String consumoDisco = String.format("%.2f", recurso.getConsumoDisco());
+       
+      
+        json.put("text", String.format("Olá! Aqui é a Sys, bot da Sysight! "
+                + "Estou aqui para mostrar algumas informações sobre seu sistema. "
+                + "Vamos lá:\n"
+                + "Data e Horario: %s\n"
+                + "Memoria RAM: %s%%\n"
+                + "Processador: %s%%\n"
+                + "Discos: %s%%\n",
+                momento,
+                consumoRam,
+                consumoCpu,
+                consumoDisco)); 
+        Slack.enviarMensagem(json);
+        
+        // Texto padrão para insert
+        String insertStatement = "INSERT INTO Recurso (fkEquipamento, consumoRam, consumoCpu, consumoDisco, momento) VALUES (?, ?, ?, ?, ?)";
+        
+                // Utilizamos o comando "update" para inserir e/ou atualizar registros.
+                // Podemos utilizar variavéis como o exemplo abaixo:
+                //ALTERE A FK DE ACORDO COM SEU EQUIPAMENTO
+                config.update(insertStatement, 2,consumoRam, consumoCpu, consumoDisco, momento);
+
+        System.out.println("Dados de recursos inseridos na Azure! :D");
+        System.out.println("Dados de recursos inseridos no Mysql! :D");
+     }
+}
---
+++ sysight/src/main/java/br/com/sysight/Slack.java
@@ -0,0 +1,26 @@
+package br.com.sysight;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import org.json.JSONObject;
+
+public class Slack {
+    private static HttpClient client = HttpClient.newHttpClient();
+    private static final String URL = "https://hooks.slack.com/services/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";
+    
+    public static void enviarMensagem(JSONObject content) throws IOException, InterruptedException{
+        HttpRequest request = HttpRequest.newBuilder(
+                URI.create(URL).create(URL).create(URL))
+                .header("accept", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(content.toString()))
+                .build();
+        
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        
+        System.out.println(String.format("Status: %s", response.statusCode()));
+        System.out.println(String.format("Response: %s", response.body()));
+    }
+}
